fdb11739330a268ab632d46e96bcf20b4dd90996
tomicida <thomas.b.almeida@gmail.com>
2024-02-05 18:52:05 +0000

    turned YingYang into Multi

--- maven/portais/src/main/java/com/portais/Ferramentas/YinYang.java
+++ maven/portais/src/main/java/com/portais/Ferramentas/YinYang.java
@@ -7,6 +7,8 @@ public class YinYang extends Ferramenta{
     public YinYang(Portal portal, int id){
         super("Yin Yang", portal, id);
 
+        this.multi = true;
+
         this.resultados.add("Equilíbrio");
         this.resultados.add("Falta Yang/Racional");
         this.resultados.add("Falta Yin/Emocional");
@@ -23,7 +25,9 @@ public class YinYang extends Ferramenta{
         this.ações.add("Nivelação da energia passiva com a activa");
         this.ações.add("Nivelação da energia ativa com a passiva");
         
-        this.Decreto = "Eu ativo a Matriz do Yin Yang para que limpe a informação anomala no relacionamento aqui tratado de (nome do consulente) e todos os seus registros no campo morfogenético e mórfico. Removendo, destruindo e transmutando todas as frequências de má informação da anomalia por (resultado) e promovendo (ação) . Fixo agora no (campo) a ação de (ação). Pulsa no campo, pulsa no campo, pulsa no campo.";
+        this.repeatString = "por (resultado) e promovendo (ação), ";
+        this.repeatString2= "(ação), ";
+        this.Decreto = "Eu ativo a Matriz do Yin Yang para que limpe a informação anomala no relacionamento aqui tratado de (nome do consulente) e todos os seus registros no campo morfogenético e mórfico. Removendo, destruindo e transmutando todas as frequências de má informação da anomalia (next). Fixo agora no (campo) a ação de (next2). Pulsa no campo, pulsa no campo, pulsa no campo.";
 
     }
     
